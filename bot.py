dba95d876d7619cdc5774847dbcc1e2539e487e8
Alec Maier <Maier.144@osu.edu>
2019-09-30 14:56:43 -0400

    added instruction to scroll the table on mobile

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ MAX_CLAIMS = 3
 EMPTY_QUERY_ERROR = "You did not provide a query from which to filter claims!\n\n"
 MONITORED_SUBS = "politics+news+worldnews+Liberal+Conservative+botwatch+Libertarian"
 API_ERROR = "There was an issue with fetching claims for your query. :( You could try again another time."
-replyHeader = "Attempting to curate up to 3 relevant fact-checked claims based on your query. Irrelevant? Try being more specific.\n\n"
+replyHeader = "Attempting to curate up to 3 relevant fact-checked claims based on your query. On mobile, scroll the table to the side:\n\n"
 replyFooter = "\n\n_I am a bot utilizing Google's fact check exploration tool. I currently monitor r/politics, r/news, r/worldnews, r/Liberal, and r/Conservative (banned)._ \n\n^[Code/Documentation](https://github.com/AlecM33/fact-check-bot)"
 
 def main():
